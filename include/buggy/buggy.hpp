36af1bbb3f529aa3a626c0ba9258f0d9bdd62f95
Aaron Cox <aaron@greymass.com>
2025-04-18 13:57:53 -0700

    Formatting

--- include/buggy/buggy.hpp
+++ include/buggy/buggy.hpp
@@ -8,27 +8,25 @@ using namespace std;
 
 namespace buggy {
 
-struct buggy_struct
-{
-   eosiosystem::eosio_global_state global;
-   eosiosystem::powerup_state      powerup;
-   eosiosystem::exchange_state     ram;
-   eosiosystem::rex_pool           rex;
+struct buggy_struct {
+  eosiosystem::eosio_global_state global;
+  eosiosystem::powerup_state powerup;
+  eosiosystem::exchange_state ram;
+  eosiosystem::rex_pool rex;
 };
 
-class [[eosio::contract("buggy")]] buggy : public contract
-{
+class [[eosio::contract("buggy")]] buggy : public contract {
 public:
-   using contract::contract;
+  using contract::contract;
 
-   [[eosio::action, eosio::read_only]] buggy_struct foo();
-   using foo_action = eosio::action_wrapper<"foo"_n, &buggy::foo>;
+  [[eosio::action, eosio::read_only]] buggy_struct foo();
+  using foo_action = eosio::action_wrapper<"foo"_n, &buggy::foo>;
 
 private:
-   eosiosystem::eosio_global_state get_global();
-   eosiosystem::powerup_state      get_powerup();
-   eosiosystem::exchange_state     get_rammarket();
-   eosiosystem::rex_pool           get_rex_pool();
+  eosiosystem::eosio_global_state get_global();
+  eosiosystem::powerup_state get_powerup();
+  eosiosystem::exchange_state get_rammarket();
+  eosiosystem::rex_pool get_rex_pool();
 };
 
 } // namespace buggy
--- src/buggy.cpp
+++ src/buggy.cpp
@@ -2,48 +2,43 @@
 
 namespace buggy {
 
-eosiosystem::eosio_global_state buggy::get_global()
-{
-   eosiosystem::global_state_singleton global_table("eosio"_n, "eosio"_n.value);
-   auto                                global = global_table.get_or_default();
-   return global;
+eosiosystem::eosio_global_state buggy::get_global() {
+  eosiosystem::global_state_singleton global_table("eosio"_n, "eosio"_n.value);
+  auto global = global_table.get_or_default();
+  return global;
 }
 
-eosiosystem::powerup_state buggy::get_powerup()
-{
-   eosiosystem::powerup_state_singleton powerup_table("eosio"_n, 0);
-   auto                                 powerup = powerup_table.get_or_default();
-   return powerup;
+eosiosystem::powerup_state buggy::get_powerup() {
+  eosiosystem::powerup_state_singleton powerup_table("eosio"_n, 0);
+  auto powerup = powerup_table.get_or_default();
+  return powerup;
 }
 
-eosiosystem::exchange_state buggy::get_rammarket()
-{
-   eosiosystem::exchange_state ram;
-   eosiosystem::rammarket      rammarket_table("eosio"_n, "eosio"_n.value);
-   auto                        rammarket_itr = rammarket_table.find(symbol("RAMCORE", 4).raw());
-   if (rammarket_itr != rammarket_table.end()) {
-      ram = *rammarket_itr;
-   }
-   return ram;
+eosiosystem::exchange_state buggy::get_rammarket() {
+  eosiosystem::exchange_state ram;
+  eosiosystem::rammarket rammarket_table("eosio"_n, "eosio"_n.value);
+  auto rammarket_itr = rammarket_table.find(symbol("RAMCORE", 4).raw());
+  if (rammarket_itr != rammarket_table.end()) {
+    ram = *rammarket_itr;
+  }
+  return ram;
 }
 
-eosiosystem::rex_pool buggy::get_rex_pool()
-{
-   eosiosystem::rex_pool       rex;
-   eosiosystem::rex_pool_table rex_table("eosio"_n, "eosio"_n.value);
-   auto                        rex_itr = rex_table.begin();
-   if (rex_itr != rex_table.end()) {
-      rex = *rex_itr;
-   }
-   return rex;
+eosiosystem::rex_pool buggy::get_rex_pool() {
+  eosiosystem::rex_pool rex;
+  eosiosystem::rex_pool_table rex_table("eosio"_n, "eosio"_n.value);
+  auto rex_itr = rex_table.begin();
+  if (rex_itr != rex_table.end()) {
+    rex = *rex_itr;
+  }
+  return rex;
 }
 
-[[eosio::action, eosio::read_only]] buggy_struct buggy::foo()
-{
-   return buggy_struct{
-      .global  = get_global(),
+[[eosio::action, eosio::read_only]] buggy_struct buggy::foo() {
+  return buggy_struct{
+      .global = get_global(),
       .powerup = get_powerup(),
-   };
+  };
 }
 
 } // namespace buggy
